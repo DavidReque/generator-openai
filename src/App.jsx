35480f8ea78ade5ea4049c06cfa7c5746f43d95e
David Requeno <davidrequeno52@gmail.com>
2023-03-21 19:07:12 -0600

    0.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 function App() {
-  const [image, setImage] = useState(
-    "https://oaidalleapiprodscus.blob.core.windows.net/private/org-aPrFAxJJBudZ3SR3XXFxJPxn/user-GTN38Nhk4sLzBRWQFRNUDpyy/img-67kNHXPe61ivUl8z0GeOSLEf.png?st=2023-03-21T18%3A26%3A45Z&se=2023-03-21T20%3A26%3A45Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-21T15%3A38%3A50Z&ske=2023-03-22T15%3A38%3A50Z&sks=b&skv=2021-08-06&sig=silGDG1U%2BDqHAZ82lA7QbWgAmd0v0We1akuol82oYCo%3D"
-  );
+  const [image, setImage] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [prompt, setPrompt] = useState("");
 
